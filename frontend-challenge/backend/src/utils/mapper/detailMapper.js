c7bdf98e9be66b5ac688c4b6f3ccb29c2c4fa837
Franco Mazzoni <franco.mazzoni@despegar.com>
2020-10-17 16:57:21 -0300

    Change image in detail

--- frontend-challenge/backend/src/utils/mapper/detailMapper.js
+++ frontend-challenge/backend/src/utils/mapper/detailMapper.js
@@ -13,8 +13,12 @@ async function mapDetail(item, description, res) {
 
 async function mapDetailItem(item, description, res) {
     let freeShipping = item.shipping ? item.shipping.free_shipping : false;
+
+    //Se usa la primera imágen que viene en el servicio. Si no viene ninguna se muestra el thumbnail
+    let picture = (item.pictures && item.pictures.length > 0) ? item.pictures[0].secure_url : item.secure_thumbnail;
+
     //Aclaración: se agrega categories a la respuesta para poder armar el breadcrumb del ítem en la página de detail
-    return new ItemModel(item.id, item.title, await mapPrice(item.price, item.currency_id, res), item.thumbnail, item.condition, freeShipping,
+    return new ItemModel(item.id, item.title, await mapPrice(item.price, item.currency_id, res), picture, item.condition, freeShipping,
         null, item.sold_quantity, await mapCategories(item.category_id), description.plain_text).toJson();
 
 }
